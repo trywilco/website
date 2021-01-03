ce4792644b6f0358ad917fa25ec2fb8cf9e5fa4a
Alon Carmel <alon.carmel@gmail.com>
2021-01-03 10:24:31 +0200

    wilco website

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $('.btnjoin').click(function(){
+
+        var fullname = $('#fullname').val();
+        var email = $('#email').val();
+        var comapnyname = $('#companyname').val();
+
+        $('#formbody').hide();
+        $('#thankyoubox').show();
+    
+        /* Do what ever you want here, i suggest connecting to Zapier and add to airtable */
+
+    })
+
+})
